f66f1145a693f318f9c194705e828d558f838a0b
Maciej Jozwiak <maciej@mjozwiak.com>
2014-06-23 20:04:18 +0200

    commit final app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-aaaa
+worldcup-data
+=========
+
+Simple app written in Node.js and using MongoDB to scrape the data about players participating in the World Cup 2014. Data taken from the [Guardian magazine], tweaked spreadsheet available [here]. All the players' info enhanced with the information offered by [Freebase].
+
+[Guardian magazine]:http://www.theguardian.com/football/datablog/2014/jun/06/world-cup-squads-rosters-broken-down-club-age-height
+[here]:https://docs.google.com/spreadsheets/d/1_Rl2IXYokCdjG8yyLtZeAL1z8sB7fglxCL3VQKpZr4M/edit#gid=0
+[Freebase]:https://www.freebase.com/
+
---
+++ app.js
@@ -0,0 +1,129 @@
+var GoogleSpreadsheets = require("google-spreadsheets");
+var googleapis = require('googleapis');
+var freebase = require('freebase');
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/worldcup');
+
+// spreadsheet with player data
+var spreadsheet_id = "1_Rl2IXYokCdjG8yyLtZeAL1z8sB7fglxCL3VQKpZr4M";
+
+var collection = db.get('players');
+var number_players = 0;
+
+var running = 0;
+var limit = 4;
+
+var spreadsheet_data;
+
+function launcher(){
+	while(running < limit && Object.keys(spreadsheet_data).length > 0) {
+	
+	var first_key = Object.keys(spreadsheet_data)[0];
+	var row = spreadsheet_data[first_key];
+	delete spreadsheet_data[first_key];
+
+	getPlayerInfo(row, function(result) {
+			
+		// Insert to the DB
+		collection.insert(result, function (err, doc) {
+			if (err) {
+				console.log("Problem occurred while trying to add data to database... Error: "+err.message); 
+			}
+		});
+		
+		running--;
+		if(Object.keys(spreadsheet_data).length > 0) {
+			launcher();
+			} else if(running == 0) {
+				finish();
+			}
+		});
+	running++;
+	} 
+}
+
+
+function loadSpreadsheet(){
+	GoogleSpreadsheets({
+		key: spreadsheet_id
+	}, function(err, spreadsheet) {
+		spreadsheet.worksheets[0].cells({
+			range: "A462:I462"
+		}, function(err, cells) {
+			spreadsheet_data = cells['cells'];
+			launcher();
+		});
+	}); 
+}
+
+function finish(){
+	console.log("FINISHED IMPORTING DATA");
+	process.exit(0);
+}
+
+function getPlayerInfo(row, callback){
+
+	var insert_query;
+							
+	// get info about player from Freebase
+	freebase.topic(row[1]['value'], {}, function (r) {
+	
+		insert_query = {						
+			"player" : row[1]['value'],
+			"position" : row[2]['value'],
+			"number" : row[3]['value'],
+			"club" : row[4]['value'],
+			"nationality" : row[6]['value'],
+			"birthday" : row[5]['value'],
+			"caps" : row[7]['value'],
+			"goals" : row[8]['value'],
+			"plays_at_home" : row[9]['value']
+		}
+		
+		console.log(r['property']['/common/topic/description']);
+		
+		var info, img, website, height, weight;
+
+		try {
+			info = r['property']['/common/topic/description']['values'][0]['value'];
+			insert_query.info = info;
+		} catch(e) {
+			console.log("Cannot find description for player: "+row[1]['value']);
+		}
+		
+		try {
+			img = r['property']['/common/topic/image']['values'][0]['id'];
+			insert_query.img = img;
+		} catch(e) {
+			console.log("Cannot find image for player: "+row[1]['value']);
+		}
+		
+		try {
+			website = r['property']['/common/topic/official_website']['values'][0]['value'];
+			insert_query.website = website;
+		} catch(e) {
+			console.log("Cannot find website for player: "+row[1]['value']);
+		}
+		
+		try {
+			height = r['property']['/people/person/height_meters']['values'][0]['value'];
+			insert_query.height = height;
+		} catch(e) {
+			console.log("Cannot find height for player: "+row[1]['value']);
+		}	
+		
+		try {
+			weight = r['property']['/people/person/weight_kg']['values'][0]['value'];
+			insert_query.weight = weight;
+		} catch(e) {
+			console.log("Cannot find weight for player: "+row[1]['value']);
+		}
+		
+	});
+	
+	setTimeout(function() { callback(insert_query); }, 1000);
+}
+
+loadSpreadsheet();
